46aab2ac83e57a35202f0aa0b845bce6429a7279
richard <r.gomer@soton.ac.uk>
2014-07-30 17:35:25 +0100

    Update README Add the client itself!

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-KOLOLA_JSClient
-===============
+# KOLOLA JS Client
 
-The Javascript client library for the KOLOLA API
+
+The Javascript client library for the KOLOLA API.  It's a pretty simple client (in part because the API itself is still quite simple and read-only).
+
+* TIP: In Firefox, use JSONView to pretty-print raw JSON output from the API! https://addons.mozilla.org/en-US/firefox/addon/jsonview/
+* The API is GET/POST based rather than truly RESTFUL, which makes it quite easy to query just by typing URLs into a web browser.
+
+## Instantiation
+
+
+Just do
+```javascript
+var endpoint_url = "http://www.wsdtc.deimpact.org.uk/api/1/"; // For example
+var client = new KOLOLA(endpoint_url);
+```
+
+## Querying Events/People
+
+The most powerful feature is the ability to query the event database.  The same search queries that are supported in the KOLOLA web interface are supported by the API.  This is done via the   Examples include:
+
+* "user:rcg1v07" - To get all events that involved a particular user (by username)
+* "02/2014" - To get all events in February 2014
+* "headley" - To get all events with the word "headley" in the title or description
+
+Queries are sent like so:
+```javascript
+var cbevents = function(events){ console.log("Received events", events); };
+var cbpeople = function(people){ console.log("Received people", people); };
+client.query('headley', ebevents, cbpeople);
+```
+* See the response: (http://www.wsdtc.deimpact.org.uk/api/1/?q=headley)
+
+Like the KOLOLA web interface, queries return matching events AND people (if any - and only for some types of query).  Events are given to the first callback, people are given to the second callback.
+
+## Getting a particular event (or events)
+
+Events can also be queried by their internal ID.  Use the ```getEvent()``` method to do so
+```javascript
+client.getEvent([14,53,101], function(events){ console.log("Got events", events) });
+```
+
+## Getting a particular person (or people)
+
+Like events, people can be queried by ID.  Again, specify an array of IDs to query for multiple people at the same time.
+
+```javascript
+client.getPerson([1,5,6], function(people){ console.log("Got people", people) });
+```
+
+
+## Getting the Framework
+
+KOLOLA's real strength, from an oversight perspective, is the ability to assess how activities contribute towards broader goals.  An impact framework provides a set of indicators and true-or-false statements that are used to assess whether an event contributed to that indicator.
+
+Events come back from the API with information about the statements ("features") that are associated with it.  But those features need to be compared to a framework to see which indicators they map to, and what the statement actually says.
+
+The framework information object can easily be retrieved using the getFramework() method on the client.
+```javascript
+client.getFramework(function(fw){ console.log("Got the framework!", fw); });
+```
+* See the response: (http://www.wsdtc.deimpact.org.uk/api/1/?features)
+
+## Photos
+
+The URL of photos are returned by the API for events and people.  People have a 100x100 avatar, and events photos are provided in 3 sizes:
+* ```url```: A 900pixel wide version, with original proportions maintained
+* ```thumb```: A 100pixel wide/tall version, cropped to fill the square
+* ```original```: Whatever was uploaded originally - Potentially very large!
---
+++ kolola.js
@@ -0,0 +1,147 @@
+/*
+ *  KOLOLA Javascript Client - www.kolola.net
+ *
+    The MIT License (MIT)
+
+    Copyright (c) 2014 KOLOLA Limited
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+ *
+ */
+ 
+function QuickAPIClient(endpoint)
+{
+    var self = this;
+    
+    self.username = false;
+    self.password = false;
+    
+    // Set the username/password for HTTP Authentication
+    self.setAuth = function(username, password)
+    {
+        self.username = username;
+        self.password = password;
+    }
+    
+    /**
+    * Make a request to the API
+    *
+    * cb_success: function(data, status, jqXHR) callback if opreation succeeds
+    * cb_error (optional): function(Data, status, jqXHR) callback if API returns an error
+    * cb_fail (optional, as above): Callback if something goes wrong with the request
+    */
+    self.request = function(args, cb_success, cb_error, cb_fail)
+    {
+        if(typeof cb_error == 'undefined')
+            cb_error = self.default_error;
+        
+        if(typeof cb_fail == 'undefined')
+            cb_fail = self.default_fail;
+        
+        // Parse the JSON from the API and determine if the operation was successful
+        var cb = function(data, status, xhr){
+            if(typeof data.success == 'undefined')
+            {
+                cb_fail(data, status, xhr);
+            }
+            else if(data.success)
+            {
+                cb_success(data.result, status, xhr);
+            }
+            else
+            {
+                cb_error(data, status, xhr);
+            }
+        };
+        
+        var r = $.post(endpoint, args, cb, 'json');
+        r.error(cb_fail);
+    }
+    
+    
+    /**
+     * Default callbacks for errors / failures
+     */
+    self.cb_error = function(result)
+    {
+        alert('RPC Error: ' + result.message);
+    }
+    
+    self.cb_fail = function(result)
+    {
+        alert('HTTP RPC Failed D: (' + status + ')');
+    }
+}
+
+/**
+ * Instantiate a KOLOLA object with an API endpoint
+ * eg http://www.wsdtc.deimpact.org.uk/api/1/
+ */
+function KOLOLA(endpoint)
+{
+    var self = this;
+    
+    self.client = new QuickAPIClient(endpoint);
+    
+    /**
+     * Query for events/people using the same query syntax as the KOLOLA
+     * search box.
+     *
+     * Events are passed to the first callback, people to the second
+     *
+     * eg:  "MM/YYYY" : Events by month
+     *      "user:jb1b09" : Events by user
+     *      "foo" : Events by keyword
+     *       
+     */
+    self.query = function(q, cb_events, cb_people)
+    {
+        var cb = function(data)
+        {
+            cb_events(data.events);
+            cb_people(data.people);
+        }
+        
+        self.client.request({'q': q}, cb);
+    }
+    
+    self.getEvent = function(id, cb)
+    {
+        self.client.request({'event': id}, cb);   
+    }
+    
+    /**
+     * Get a description of the assessment framework (statements + indicators)
+     */
+    self.getFramework = function(cb)
+    {
+        self.client.request({'features':null}, cb);
+    }
+    
+    /**
+     * Get information about a person (by PersonID), or persons (array of PersonIDs)
+     */
+    self.getPerson = function(id, cb)
+    {
+        self.client.request({'person': id}, cb);   
+    }
+}
+
+
+
